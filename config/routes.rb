26926037b76d3a9f03b1fbe4c00adeafe38786df
Julián R Figueroa <julian@zentrifuga.com>
2017-02-06 23:34:22 -0500

    Controllers test
    # 50-character subject line
    #
    # 72-character wrapped longer description. This should answer:
    #
    # * Why was this change necessary?
    # * How does it address the problem?
    # * Are there any side effects?
    #
    # Include a link to the ticket, if any.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   scope 'users', controller: :users do
-    post '', action: :create, as: :create_user
+    get '', action: :index, as: :users
+    post '', action: :create
     get ':id', action: :show, as: :user
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not register existing email' do
+    post users_path, params: { user: {
+      email: 'user@domain.com'
+    } }
+    assert_response 401
+  end
+
+  test 'should register basic' do
+    post users_path, params: { user: {
+      email: 'julian@zentrifuga.com'
+    } }
+    assert_response 302
+  end
+
+  test 'should register complex' do
+    post users_path, params: { user: {
+      first_name: 'Julian',
+      last_name: 'Figueroa',
+      zipcode: '123456',
+      street: 'A Wide Street',
+      country: 'Colombia',
+      email: 'julian@zentrifuga.com'
+    } }
+    assert_response 302
+  end
+
+  test 'should get user info' do
+    user = users(:one)
+    get user_path id: user.id, token: user.token
+    assert_response 200
+  end
+
+  test 'should not get user info' do
+    user = users(:one)
+    get user_path id: user.id
+    assert_response 401
+  end
 end
