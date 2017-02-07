c53943ff4c8a3993236ccd86830d19675fff5fdb
Julián R Figueroa <julian@zentrifuga.com>
2017-02-06 23:03:49 -0500

    Both routes
    # 50-character subject line
    #
    # 72-character wrapped longer description. This should answer:
    #
    # * Why was this change necessary?
    # * How does it address the problem?
    # * Are there any side effects?
    #
    # Include a link to the ticket, if any.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope 'users', controller: :users do
+    post '', action: :create, as: :create_user
+    get ':id', action: :show, as: :user
+  end
 end
