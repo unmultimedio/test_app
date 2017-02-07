086e78b67ba45ba5ebaaad28bc92a306d4530dba
Julián R Figueroa <julian@zentrifuga.com>
2017-02-06 23:24:05 -0500

    model testing
    # 50-character subject line
    #
    # 72-character wrapped longer description. This should answer:
    #
    # * Why was this change necessary?
    # * How does it address the problem?
    # * Are there any side effects?
    #
    # Include a link to the ticket, if any.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should create a new basic user' do
+    user = User.new(
+      email: 'julian@zentrifuga.com'
+    )
+    assert user.save
+  end
+
+  test 'should create a more complex user' do
+    user = User.new(
+      email: 'another_user@domain.com',
+      first_name: 'Julian',
+      last_name: 'Figueroa',
+      zipcode: '54321',
+      street: 'A Bright Street',
+      country: 'UK'
+    )
+    assert user.save
+  end
+
+  test 'should not create user without email' do
+    user = User.new(
+      first_name: 'Julian',
+      last_name: 'Figueroa',
+      zipcode: '54321',
+      street: 'A Bright Street',
+      country: 'UK'
+    )
+    assert_not user.save
+  end
 end
